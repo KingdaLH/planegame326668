a23425042e8656b86d4aee120de611d636d30cb2
David Poels <326668@student.mboutrecht.nl>
2019-11-08 23:58:14 +0100

    Initial Commit

---
+++ Assets/Challenge 1/Scripts/MovingObstacle1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle1 : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //rotates the obstacle
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/MovingObstacle2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle2 : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public float start;
+    public float end;
+    public bool up = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        start = startPosition.y;
+
+        end = startPosition.y + 7f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if statement to move the obstacle up and down in rapid succession.
+        if (transform.position.y > end)
+        {
+            up = false;
+        }else if(transform.position.y < start)
+        {
+            up = true;
+        }
+        if (up)
+        {
+            transform.Translate(0, Time.deltaTime * 100f, 0);
+        }
+        else
+        {
+            transform.Translate(0, Time.deltaTime * -100f, 0);
+        }
+    }
+}
